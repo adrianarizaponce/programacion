90af8273ed21b354a514ab09ba8d2f9a0553699a
Adrián Ariza <ariza.poadr22@triana.salesianos.edu>
2023-11-30 14:17:02 +0100

    inicializando repositorio

---
+++ ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,35 @@
+package ejercicio3;
+
+
+	
+	
+	
+	import java.util.Arrays; 
+
+	public class Ejercicio3 {
+	    
+	    public static void main(String[] args) {
+	        
+	        int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+	        
+	        // Insert an element in 3rd position of the array (index->2, value->5)
+	        
+	        int Index_position = 2;
+	        int newValue    = 5;
+	        
+	        System.out.println("Original Array : "+Arrays.toString(my_array));     
+	        
+	        for(int i=my_array.length; i < Index_position; i++){
+	            my_array[i] = my_array[i];
+	        }
+	        my_array[Index_position-1] = newValue;
+	        System.out.println("New Array: "+Arrays.toString(my_array));
+	    }
+	}
+	
+	
+	
+	
+	
+
+
--- repositorio/src/repositorio/Principal.java
+++ repositorio/src/repositorio/Principal.java
@@ -5,6 +5,14 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("hola a todos ...");
 	}
 
 }
